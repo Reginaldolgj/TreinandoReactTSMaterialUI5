b22392dd42624135bdc33c5f39ae37e4fe7d358c
Reginaldo <reginaldolgj@gmail.com>
2023-06-10 15:12:37 -0300

    feat: add theme black

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material"
 import { Routes, Route, Navigate } from "react-router-dom"
+import { useAppThemeContext } from "../shared/context/ThemeContext"
 
 export const AppRoutes = () => {
+  const {toggleTheme} = useAppThemeContext();
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary"> tste</Button>} />
+      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}> tste</Button>} />
       <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
     </Routes>
   )
